07b0350b5d5629e5f07be485af170b49ecc8be16
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-02-07 20:18:40 -0300

    chore: Adding Cart in header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function Home() {
 	return (
 		<View className='flex-1 pt-8'>
-			<Header title='Faça seu pedido' />
+			<Header title='Faça seu pedido' cartQuantityItems={5} />
 		</View>
 	);
 }
