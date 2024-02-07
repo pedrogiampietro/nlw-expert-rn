11f76c8eba86bff0784be22fe97d65c8d38354d9
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-02-07 20:35:10 -0300

    chore: category button feat flatList to render same

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import { View, FlatList } from 'react-native';
+import { CategoryButton } from '@/components/category-button';
 import { Header } from '@/components/header';
-import { View, Text } from 'react-native';
+
+import { CATEGORIES } from '@/utils/data/products';
 
 export default function Home() {
+	const [category, setCategory] = useState(CATEGORIES[0]);
+
+	function handleCategorySelect(selectedCategory: string) {
+		setCategory(selectedCategory);
+	}
+
 	return (
 		<View className='flex-1 pt-8'>
 			<Header title='Faça seu pedido' cartQuantityItems={5} />
+
+			<FlatList
+				data={CATEGORIES}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<CategoryButton
+						title={item}
+						isSelected={item === category}
+						onPress={() => handleCategorySelect(item)}
+					/>
+				)}
+				horizontal
+				className='max-h-10 mt-5'
+				showsHorizontalScrollIndicator={false}
+				contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+			/>
 		</View>
 	);
 }
